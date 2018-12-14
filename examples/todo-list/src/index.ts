db4a5a555ea5abce2f247c59687fdeaa2c500c01
Nathan Lee <esoodev@gmail.com>
2018-12-14 18:08:21 +0900

    Added oasgraph.

--- examples/todo-list/src/index.ts
+++ examples/todo-list/src/index.ts
@@ -5,6 +5,11 @@
 
 import {TodoListApplication} from './application';
 import {ApplicationConfig} from '@loopback/core';
+import express = require('express');
+import request = require('request-promise-native');
+import graphqlHTTP = require('express-graphql');
+
+const {createGraphQlSchema} = require('oasgraph');
 
 export {TodoListApplication};
 
@@ -15,5 +20,25 @@ export async function main(options: ApplicationConfig = {}) {
 
   const url = app.restServer.url;
   console.log(`Server is running at ${url}`);
+
+  /**
+   * RUN OASGRAPH
+   */
+  let oas = await request.get(`${app.restServer.url}/openapi.json`);
+
+  const {schema, report} = await createGraphQlSchema(JSON.parse(oas), {
+    addSubOperations: true,
+  });
+  const graphqlServer = express();
+  graphqlServer.use(
+    '/graphql',
+    graphqlHTTP({
+      schema,
+      graphiql: true,
+    }),
+  );
+  graphqlServer.listen(3001);
+  console.log(`OASGraph server is running at port 3001/graphql`);
+
   return app;
 }
